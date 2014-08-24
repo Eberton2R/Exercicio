888c28e24f19e92cdb496daa403f74625cee1aa9
LIPEBORINHA <LIPEBORINHA@TECNOLIPE>
2014-08-24 11:48:17 -0300

    alteração para caps lock

--- gitTrabalho/src/br/work/git/app.java
+++ gitTrabalho/src/br/work/git/app.java
@@ -6,6 +6,7 @@ public class app {
 	public static void main(String[] args) {
 		System.out.println("arquivo 2 - philippe");
 		System.out.println("arquivo 3 - philippe");
+		System.out.println("ARQUIVO 3 ");
 
 	}
 
